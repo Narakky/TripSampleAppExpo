efae72dcf1e1778e72ae19bb066986433c49ddb2
ie-naraki <36832932+ie-naraki@users.noreply.github.com>
2019-05-28 11:20:15 +0900

    ホーム画面にリストを追加

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,133 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { ButtonGroup } from 'react-native-elements';
+import { ButtonGroup, ListItem } from 'react-native-elements';
+import { ScrollView } from 'react-native-gesture-handler';
+
+const ALL_INDEX = 0;
+const GREAT = 'sentiment-very-satisfied';
+const GREAT_INDEX = 1;
+const GREAT_COLOR = 'red';
+const GOOD = 'sentiment-satisfied';
+const GOOD_INDEX = 2;
+const GOOD_COLOR = 'orange';
+const POOR = 'sentiment-dissatisfied';
+const POOR_INDEX = 3;
+const POOR_COLOR = 'blue';
+
+const allReviewsTmp = [
+  {
+    country: 'USA',
+    dateFrom: 'Jan/15/2018',
+    dateTo: 'Jan/25/2018',
+    imageURIs: [
+      require('../assets/add_image_placeholder.png'),
+      require('../assets/add_image_placeholder.png'),
+      require('../assets/add_image_placeholder.png'),
+    ],
+    rank: GREAT
+  },
+  {
+    country: 'USA',
+    dateFrom: 'Feb/15/2018',
+    dateTo: 'Feb/25/2018',
+    imageURIs: [
+      require('../assets/add_image_placeholder.png'),
+      require('../assets/add_image_placeholder.png'),
+      require('../assets/add_image_placeholder.png'),
+    ],
+    rank: GOOD
+  },
+  {
+    country: 'USA',
+    dateFrom: 'Mar/15/2018',
+    dateTo: 'Mar/25/2018',
+    imageURIs: [
+      require('../assets/add_image_placeholder.png'),
+      require('../assets/add_image_placeholder.png'),
+      require('../assets/add_image_placeholder.png'),
+    ],
+    rank: POOR
+  },
+];
 
 class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
   
     this.state = {
-       selectedIndex: 0,
+       selectedIndex: ALL_INDEX,
     };
   }
   
   onButtonGroupPress = (selectedIndex) => {
     this.setState({ selectedIndex });
+  }
+
+  renderReviews() {
+    let reviewRank;
+
+    switch (this.state.selectedIndex) {
+      case GREAT_INDEX:
+        reviewRank = GREAT;
+        break;
+
+      case GOOD_INDEX:
+        reviewRank = GOOD;
+        break;
+
+      case POOR_INDEX:
+        reviewRank = POOR;
+        break;
+    
+      default:
+        break;
+    }
+
+    let rankedReviews = [];
+
+    if (this.state.selectedIndex === ALL_INDEX) {
+      rankedReviews = allReviewsTmp;
+    } else {
+      for (let i = 0 ; i < allReviewsTmp.length ; i++) {
+        if (allReviewsTmp[i].rank === reviewRank) {
+          rankedReviews.push(allReviewsTmp[i]);
+        }
+      }
+    }
+
+    return (
+      <ScrollView>
+        {rankedReviews.map((review, index) => {
+          let reviewColor;
+
+          switch (review.rank) {
+            case GREAT:
+              reviewColor = GREAT_COLOR;
+              break;
+
+            case GOOD:
+              reviewColor = GOOD_COLOR;
+              break;
+
+            case POOR:
+              reviewColor = POOR_COLOR;
+              break;
+
+            default:
+              break;
+          }
+
+          return (
+            <ListItem
+              key={index}
+              leftIcon={{ name: review.rank, color: reviewColor }}
+              title={review.country}
+              subtitle={`${review.dateFrom} ~ ${review.dateTo}`}
+            />
+          );
+        })}
+      </ScrollView>
+    );
   }
 
   render() {
@@ -30,6 +145,8 @@ class HomeScreen extends React.Component {
           selectedIndex={this.state.selectedIndex}
           onPress={this.onButtonGroupPress}
         />
+
+        {this.renderReviews()}
       </View>
     );
   }
